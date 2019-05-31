42bbf1ee6076e2071d3e23611f79a4b7b7c58cee
Diego Cruz <abeja.dact_@hotmail.com>
2019-05-31 13:06:27 -0500

    mejoro el codigo borro lo que no sribe

--- Assets/script/Weapons.cs
+++ Assets/script/Weapons.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
-
     //Manos
     public GameObject rightHand, leftHand;
     //posiciones para los poderes 
     Vector3 lastPositionRight, lastPositionLeft;
-    //gameboject para las armas y los puntos donde sale los poderes
+    //gameboject para las armas y los pñuntos donde sale los poderes
     public GameObject rightWeapon, leftWeapon, rightWeaponAlt, magicLaunchPoint;
     //poderes
     public GameObject fireMagic;
+
     GameObject currentMagic;
     //tiempo de espera entre cambios de armas y poderes
     public float weaponCooldown, magicCooldown = 0.0f;
     //cooldown
     public const float WEAPON_COOLDOWN_TIME = 0.5f;
-    public const float MAGIC_COOLDOWN_TIME = 2.0f;
+    public const float MAGICN_COOLDOWN_TIME = 2.0f;
+
     //Activar escudo
     public bool shieldActive = false;
-
-    public AudioClip fireClip;
 
     // Use this for initialization
     void Start()
@@ -33,57 +32,36 @@ public class Weapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //TODO: gatillo izquierdo para cubrirnos con el escudo 
         weaponCooldown += Time.deltaTime;
         magicCooldown += Time.deltaTime;
 
-        if (Input.GetAxis("HTC_VIU_LeftTrigger") > 0.1 &&
-           leftWeapon.activeInHierarchy)
+        if (Input.GetAxis("HTC_VIU_LeftTrigger") > 0.1 && leftWeapon.activeInHierarchy)
         {
+            //TODO: gatillo izquierdo para cubrirnos con el escudo 
             shieldActive = true;
+
+
         }
         else
         {
             shieldActive = false;
         }
-
-        if (Input.GetAxis("HTC_VIU_RightTrigger") > 0.1 &&
-           magicCooldown > MAGIC_COOLDOWN_TIME)
+        if (Input.GetAxis("HTC_VIU_RightTrigger") > 0.1)
         {
-            
-            if (currentMagic != null)
-            {
-                magicCooldown = 0;
-
-                Vector3 force = 20.0f * (rightHand.transform.position -
-                                       lastPositionRight) / Time.deltaTime;
-                currentMagic.transform.parent = null;
-                currentMagic.GetComponent<Rigidbody>().constraints =
-                    RigidbodyConstraints.None;
-                currentMagic.GetComponent<Rigidbody>().
-                            AddForce(force, ForceMode.Impulse);
-
-                currentMagic.GetComponent<AudioSource>().PlayOneShot(fireClip);
-
-                Invoke("LoadMagic", MAGIC_COOLDOWN_TIME);
-            }
-
+            //TODO: gatillo derecho dispara fuego si tenemos la vara seleccionada
         }
-
-        if (Input.GetAxis("HTC_VIU_LeftGrip") > 0.1 &&
-           weaponCooldown > WEAPON_COOLDOWN_TIME)
-           //TODO: mostrar/ocultar el escudo
+        if (Input.GetAxis("HTC_VIU_LeftGrip") > 0.1  && weaponCooldown > WEAPON_COOLDOWN_TIME)
         {
+            //TODO: mostrar/ocultar el escudo
             weaponCooldown = 0;
             leftWeapon.SetActive(!leftWeapon.activeInHierarchy);
-        }
 
-        if (Input.GetAxis("HTC_VIU_RightGrip") > 0.1 &&
-           weaponCooldown > WEAPON_COOLDOWN_TIME)
-        //TODO: gatillo derecho dispara fuego si tenemos la vara seleccionada
+        }
+        if (Input.GetAxis("HTC_VIU_RightGrip") > 0.1 && weaponCooldown > WEAPON_COOLDOWN_TIME)
         {
+            //TODO: cambiar entre espada y vara
             weaponCooldown = 0;
-            rightWeapon.SetActive(!rightWeapon.activeInHierarchy);
+            rightWeapon.SetActive(!rightHand.activeInHierarchy);
             rightWeaponAlt.SetActive(!rightWeaponAlt.activeInHierarchy);
 
             if (rightWeaponAlt.activeInHierarchy)
@@ -94,21 +72,13 @@ public class Weapons : MonoBehaviour
             {
                 Destroy(currentMagic);
             }
+
         }
 
-
-        lastPositionRight = rightHand.transform.position;
-        lastPositionLeft = leftHand.transform.position;
     }
 
     void LoadMagic()
     {
-        if (currentMagic != null)
-        {
-            Destroy(currentMagic);
-        }
 
-        currentMagic = Instantiate(fireMagic, magicLaunchPoint.transform);
     }
-
 }
